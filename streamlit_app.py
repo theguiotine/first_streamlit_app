7a03cd632471238171c4626dca1ab6515ddbedce
Esteban Guio <69354804+theguiotine@users.noreply.github.com>
2022-11-13 17:02:30 -0600

    Update streamlit_app.py
    fruitchoice created to fill user selection sent to api to call data

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,9 +22,11 @@ streamlit.dataframe(fruits_to_show)
 
 #new section
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # normalizes json pull
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
