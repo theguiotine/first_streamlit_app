8dc75f7a507310d2b610171174e23913f083fe14
Esteban Guio <69354804+theguiotine@users.noreply.github.com>
2022-11-13 16:58:53 -0600

    Update streamlit_app.py
    normalized json
    display as dataframe rather than txt

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# normalizes json pull
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# displays outpu as df instead of txt
+streamlit.dataframe(fruityvice_normalized)
