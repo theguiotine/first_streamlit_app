4a404461c3f89834797e0942c74c408f2ca7fa70
Esteban Guio <69354804+theguiotine@users.noreply.github.com>
2022-11-21 21:07:10 -0500

    Update streamlit_app.py
    Trying

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,12 +37,11 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
+my_cur.execute("SELECT * from fruit_load_list" + "insert into fruit_load_list values (add_my_fruit)")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-insert into fruit_load_list values (add_my_fruit)
 streamlit.write('Thanks for adding ', add_my_fruit)
