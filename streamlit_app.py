73200bbfbbff98bda76ae7c2cee091ec3e40dcfc
Esteban Guio <69354804+theguiotine@users.noreply.github.com>
2022-11-21 20:37:00 -0500

    Update streamlit_app.py
    Ability to add a fruit enabled

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,10 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
+my_cur.execute("SELECT * from fruit_load_list" + add_my_fruit)
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
