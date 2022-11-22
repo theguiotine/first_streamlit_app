4df11eeb9723e438b11446acec5ab381102efe15
Esteban Guio <69354804+theguiotine@users.noreply.github.com>
2022-11-21 21:14:14 -0500

    Update streamlit_app.py
    Stopping afte rfruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # displays outpu as df instead of txt
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
+
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
