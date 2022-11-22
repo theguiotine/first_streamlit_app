02e20031547358a11ad1c5ccad41e6d7d5c5984b
Esteban Guio <69354804+theguiotine@users.noreply.github.com>
2022-11-21 20:11:27 -0500

    Update streamlit_app.py
    Having SF pull in python libraries needed

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,3 +32,5 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # displays outpu as df instead of txt
 streamlit.dataframe(fruityvice_normalized)
+
+import snowflake.connector
